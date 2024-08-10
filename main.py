00ae1076d4a50ae83530c1adbe2fb20d96af803b
abhishekY2401 <abhiyd2401@gmail.com>
2024-08-10 19:42:55 +0530

    add logger for debugging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import logging
 import pathlib
 import textwrap
 import fitz
@@ -8,40 +9,80 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 
 load_dotenv()
+# Configure logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-print()
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# Get the API key from the environment
+api_key = os.getenv("GOOGLE_API_KEY")
+if api_key is None:
+    logging.error("GOOGLE_API_KEY is not set in the environment variables.")
+    st.error(
+        "API key is missing. Please set the API key in the environment variables.")
+else:
+    genai.configure(api_key=api_key)
 
 # Function to load OpenAI model and get respones
 
 
 def get_gemini_response(prompt, text=None, image=None):
+    logging.info("Starting LLM API call")
     model = genai.GenerativeModel('gemini-1.5-flash')
-    # Prepare input data for the Gemini model
     inputs = [prompt]
     if text:
+        logging.info(f"Text length: {len(text)} characters")
+        text = text[:5000]  # Limiting text length
         inputs.append(text)
     if image:
         inputs.append(image)
-    response = model.generate_content(inputs)
-    return response.text
+    try:
+        response = model.generate_content(inputs)
+        logging.info("LLM API call successful")
+        return response.text
+    except Exception as e:
+        logging.error(f"Failed to generate response: {e}")
+        st.error(f"Failed to generate response: {e}")
+        return None
 
 
 def extract_text_from_pdf(uploaded_file):
-    # Extract text from the PDF file
+    logging.info("Starting PDF text extraction")
     pdf_text = ""
-    with fitz.open(stream=uploaded_file.read(), filetype="pdf") as pdf_document:
-        for page_num in range(len(pdf_document)):
-            page = pdf_document.load_page(page_num)
-            pdf_text += page.get_text()
-
+    try:
+        with fitz.open(stream=uploaded_file.read(), filetype="pdf") as pdf_document:
+            for page_num in range(len(pdf_document)):
+                logging.info(f"Processing page {page_num}")
+                page = pdf_document.load_page(page_num)
+                pdf_text += page.get_text()
+        logging.info("PDF text extraction completed")
+    except Exception as e:
+        logging.error(f"Failed to process PDF: {e}")
+        st.error(f"Failed to process PDF: {e}")
+        return None
     return pdf_text
 
 
-# Function to process image file
-def process_image_file(uploaded_file):
-    image = Image.open(uploaded_file)
-    return image
+# Function to process uploaded images
+def input_image_setup(uploaded_file):
+    logging.info("Setting up image input")
+    if uploaded_file is not None:
+        try:
+            bytes_data = uploaded_file.getvalue()
+            image_parts = [
+                {
+                    "mime_type": uploaded_file.type,
+                    "data": bytes_data
+                }
+            ]
+            logging.info("Image input setup successful")
+            return image_parts
+        except Exception as e:
+            logging.error(f"Failed to process image: {e}")
+            st.error(f"Failed to process image: {e}")
+            return None
+    else:
+        logging.warning("No image file uploaded")
+        raise FileNotFoundError("No file uploaded")
 
 
 # initialize our streamlit app
@@ -52,27 +93,37 @@ st.header("Invoice Details Application")
 uploaded_file = st.file_uploader(
     "Choose a file", type=["jpg", "jpeg", "png", "pdf"])
 
+image = None
+pdf_text = None
+
+if uploaded_file is not None:
+    file_type = uploaded_file.type.split('/')[1]
+    logging.info(f"Uploaded file type: {file_type}")
+
+    if file_type in ["jpg", "jpeg", "png"]:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image.", use_column_width=True)
+        image = input_image_setup(uploaded_file)
+    elif file_type == "pdf":
+        pdf_text = extract_text_from_pdf(uploaded_file)
+
 submit = st.button("Analyze Invoice")
 input_prompt = """
                You are an expert in understanding invoices.
                You will receive input images and pdfs as invoices &
                you will have to only extract customer, product details and total amount
-               based on the input image. Please represent the above mentioned data in proper readable 
-               format.
+               based on the input. Please represent the above mentioned data in proper readable 
+               format. Also dont provide other details
                """
+
 # If submit button is clicked
-if submit and uploaded_file is not None:
-    if uploaded_file.type == "application/pdf":
-        st.write("PDF file uploaded.")
-        # Extract text from PDF
-        pdf_text = extract_text_from_pdf(uploaded_file)
-        response = get_gemini_response(input_prompt, text=pdf_text)
-        st.subheader("The Response is")
-        st.write(response)
+if submit:
+    if image or pdf_text:
+        response = get_gemini_response(
+            input_prompt, text=pdf_text, image=image)
+        if response:
+            st.subheader("The Response is")
+            st.write(response)
     else:
-        st.write("Image file uploaded.")
-        image = process_image_file(uploaded_file)
-        response = get_gemini_response(input_prompt, image=image)
-        st.image(image, caption="Uploaded Image.", use_column_width=True)
-        st.subheader("The Response is")
-        st.write(response)
+        logging.warning("No file content to process")
+        st.error("No file content to process. Please upload a PDF or image.")
